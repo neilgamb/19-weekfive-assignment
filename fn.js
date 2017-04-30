1da7c1d5b7721b152aecaa49652a5fdbe6dbf755
Neilson Gamble <neilgamb@neilsons-mbp.attlocal.net>
2017-04-30 16:42:38 -0400

    Submission #1.  Need more tests

--- fn.js
+++ fn.js
@@ -1,18 +1,137 @@
 module.exports = {
     range: range,
+    map: map,
+    filter: filter,
+    reduce: reduce,
+    all: all,
+    some: some,
+    find: find,
 }
 
 
+// ===== fn 1 | range =======================================
 
 function range (min, max){
 
-let nums = [];
+    let nums = [];
 
     for(let i = min; i <= max; i++){
         nums.push(i);
     }
 
+    if(min > max){
+        return null;
+    }
+
+    if(typeof(min) != 'number' || typeof(max) != 'number'){
+        return null;
+    }
+
     return nums;
 }
 
-console.log(range(5, 10));
+// ===== fn 2 | map =======================================
+
+function map (array, func){
+
+    let returnArray = [];
+
+    for (let i = 0; i < array.length; i++){
+        returnArray.push(func(array[i]));
+    }
+
+    return returnArray;
+}
+
+// ===== fn 3 | filter =======================================
+
+function filter (array, func){
+
+    let returnArray = [];
+
+    for (let i = 0; i < array.length; i++){
+
+        if (func(array[i]) === true){
+            returnArray.push(array[i]);
+        }
+    }
+    
+    return returnArray;
+}
+
+// ===== fn 4 | reduce =======================================
+
+function reduce(array, func, start){
+
+    let previous = start;
+
+    for(let i = 0; i < array.length; i++){
+        func(previous, array[i]);
+        previous = func(previous, array[i]);
+    }
+
+    return previous;
+}
+
+// ===== fn 5 | all =======================================
+
+function all(array, func){
+
+    let arrayTF = [];
+    let result = true;
+
+    for(let i = 0; i < array.length; i++){
+        if(func(array[i]) === true){
+            arrayTF.push(true);
+        } else {
+            arrayTF.push(false);
+        }
+    }
+
+    for (let p = 0; p < arrayTF.length; p++){
+        if(arrayTF[p] != true){
+            result = false;
+        }
+    }
+
+    return result;
+}   
+
+// ===== fn 6 | some =======================================
+
+function some(array, func){
+
+    let arrayTF = [];
+    let result = false;
+
+    for(let i = 0; i < array.length; i++){
+        if(func(array[i]) === true){
+            arrayTF.push(true);
+        } else {
+            arrayTF.push(false);
+        }
+    }
+
+    for (let p = 0; p < arrayTF.length; p++){
+        if(arrayTF[p] === true){
+            result = true;
+        }
+    }
+
+    return result;
+}   
+
+// ===== fn 7 | find =======================================
+
+function find (array, func){
+
+for (let i = 0; i < array.length; i++){
+    if(func(array[i])){
+        return array[i];
+    }
+
+}
+    return false;
+
+
+}
--- test_fn.js
+++ test_fn.js
@@ -1,13 +1,70 @@
 
 // Import 'functions' object from fn.js
-// Install ava node_modules in directory
-let functions = require('./fn');
+let fn = require('./fn');
 
-test ('descending', function (t){
-    t.deepEqual(practice.descending(14), 41);
-    t.deepEqual(practice.descending(5614), 6541);
-    t.deepEqual(practice.descending(90), 90);
-    t.deepEqual(practice.descending(2), 2);
-    t.deepEqual(practice.descending(11), 11);
+// Install ava node_modules in directory and import 'test' (?)
+import test from 'ava';
 
+// ===== fn 1 | range =======================================
+test ('range', function (t){
+    t.deepEqual(fn.range(5, 10), [5, 6, 7, 8, 9, 10]);
+    t.deepEqual(fn.range(5, 5), [5]);
+    t.deepEqual(fn.range(5, 3), null);
+    t.deepEqual(fn.range(-2, 4), [-2, -1, 0, 1, 2, 3, 4]);
+    t.deepEqual(fn.range('x', 2), null);
+    t.deepEqual(fn.range(2, 'x'), null);
+});
+
+// ===== fn 2 | map =======================================
+test ('map', function (t){
+    function square(x) {return x * x;}
+    t.deepEqual(fn.map([2, 4, 5], square), [4, 16, 25]);
+
+    function hello(name) {return "Hello, "+name;}
+    t.deepEqual(fn.map(['John', 'Tim', 'Julie'], hello), ['Hello, John', 'Hello, Tim', 'Hello, Julie']);
+});
+
+// ===== fn 3 | filter =======================================
+test ('filter', function (t){
+    function even(x) {return x % 2 === 0;}
+    t.deepEqual(fn.filter([1, 2, 3], even), [2]);
+});
+
+// ===== fn 4 | reduce =======================================
+test ('reduce', function (t){
+    function tallest(tall, next) {if (tall > next) {return tall} else {return next}};
+    t.deepEqual(fn.reduce([6, 9, 10, 4], tallest, 0), 10);
+
+    function longestWord(first, second) {if (first.length > second.length) {return first} else {return second}};
+    t.deepEqual(fn.reduce(['longest', 'lil', 'word'], longestWord, 0), 'longest');
+
+    function sum(first, second) {return first + second};
+    t.deepEqual(fn.reduce([6, 9, 10, 4], sum, 0), 29);
+
+    function join(first, second) {return first + '' +second};
+    t.deepEqual(fn.reduce(['Dick','Jane', 'Tim'], join, ''), 'DickJaneTim');
+});
+
+// ===== fn 5 | all =======================================
+test ('all', function (t){
+    function divisByThree(num) {return num % 3 === 0;}
+    t.deepEqual(fn.all([9, 6, 3], divisByThree), true);
+    t.deepEqual(fn.all([2, 6, 9, 300], divisByThree), false);
+    t.deepEqual(fn.all([2, 4, 300], divisByThree), false);
+});
+
+// ===== fn 6 | some =======================================
+test ('some', function (t){
+    function divisByThree(num) {return num % 3 === 0;}
+    t.deepEqual(fn.some([3, 6, 9, 300], divisByThree), true);
+    t.deepEqual(fn.some([2, 6, 9, 300], divisByThree), true);
+    t.deepEqual(fn.some([2, 4, 300], divisByThree), true);
+    t.deepEqual(fn.some([2, 4, 5], divisByThree), false);
+});
+
+// ===== fn 7 | find =======================================
+test ('find', function (t){
+    function startWithS(name) {return (name[0] === 's' || name[0] === 'S')}
+
+    t.deepEqual(fn.find(['Peg', 'Tara', 'Smith', 'Samsonite', 'Kip'], startWithS), 'Smith');
 });
